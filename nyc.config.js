eb164ac0f727f025bbc274eed71d1d3afaaaad4a
OtaiLotfi <otailotfi26@gmail.com>
2025-08-02 10:28:40 +0100

    refactor

--- nyc.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reporter: ['lcov', 'text'],
-  extension: ['.ts'],
-  include: ['otaiE2ETests/**/*.ts'],
-  exclude: ['**/*.spec.ts', '**/*.test.ts', '**/config.ts'],
-  all: true,
-  require: ['ts-node/register']
-};
